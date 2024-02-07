6a2f5129c4e0cd7269e7cac13a03b616bee10a3c
Mo Kamali <mokam1997@gmail.com>
2024-02-07 14:50:24 +0000

    This is example of domain B

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       await fetch('/api/samesite/default');
       setLoading(false); // Set loading to false when fetch is completed
       // Optionally, you can do something with the fetched data here
+      
     }
     fetchData();
   }, []);
@@ -23,13 +24,15 @@ export default function Home() {
       <h1>
         When you visit this site you will receive two default cookies.
       </h1>
-      <img
-              src="https://auth-poc-next.vercel.app/test-image.png"
-              alt="Cookie explanation from online"
-              className="dark:invert"
-              width={600}
-              height={400}
-            />
+      {!loading && (
+        <img
+          src="https://auth-poc-next.vercel.app/test-image.png"
+          alt="Cookie explanation from online"
+          className="dark:invert"
+          width={600}
+          height={400}
+        />
+      )}
             <h2>The image above is from another domain, which checks if you sent a cookie!</h2>
             <a href="https://auth-poc-next.vercel.app"> You can only request from the other domain if your cookie is sent!</a>
             <br/>
