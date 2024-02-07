5782bbc9b7f5432f592cd6ff332ee5ecf3bf9d5d
Mo Kamali <mokam1997@gmail.com>
2024-02-07 17:31:24 +0000

    This is example of domain B

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  // Function to handle button click and load the image
-  const handleButtonClick = () => {
-    setImageLoaded(true);
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
         When you visit this site, the image does not appear.
       </h1>
       <a href="https://auth-poc-next.vercel.app/"> Log in here and then come back!</a>
-      {/* Render the image only if the button has been clicked */}
-      {imageLoaded && (
         <img
           src="https://auth-poc-next.vercel.app/test-image.png"
           alt="Cookie explanation from online"
@@ -25,11 +16,6 @@ export default function Home() {
           width={600}
           height={400}
         />
-      )}
-      {/* Button to load the image */}
-      {!imageLoaded && (
-        <button onClick={handleButtonClick}>Load Image</button>
-      )}
     </main>
   );
 }
