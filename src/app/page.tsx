09756e6c0ccf60dbcc49cb61732a97f00064b1f9
Mo Kamali <mokam1997@gmail.com>
2024-02-07 17:00:34 +0000

    This is example of domain B

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from 'react';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-  useEffect(() => {
-    async function fetchData() {
-      await fetch('/api/samesite/default', {credentials: "include"});
-      setLoading(false); // Set loading to false when fetch is completed
-      // Optionally, you can do something with the fetched data here
-      
-    }
-    fetchData();
-  }, []);
+  // Function to handle button click and load the image
+  const handleButtonClick = () => {
+    setImageLoaded(true);
+  };
 
-  // Render loading state while fetching
-  if (loading) {
-    return <div>Loading...</div>;
-  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
         When you visit this site, the image does not appear.
       </h1>
       <a href="https://auth-poc-next.vercel.app/"> Log in here and then come back!</a>
-      {!loading && (
+      {/* Render the image only if the button has been clicked */}
+      {imageLoaded && (
         <img
           src="https://auth-poc-next.vercel.app/test-image.png"
           alt="Cookie explanation from online"
@@ -33,6 +25,10 @@ export default function Home() {
           width={600}
           height={400}
         />
+      )}
+      {/* Button to load the image */}
+      {!imageLoaded && (
+        <button onClick={handleButtonClick}>Load Image</button>
       )}
     </main>
   );
