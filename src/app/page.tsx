6e3969e6fb6c43b769c841b002d6c3c26367c270
Mo Kamali <mokam1997@gmail.com>
2024-02-07 14:12:59 +0000

    sameSite ??fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  // Render loading state while fetching
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  
+
   useEffect(() => {
     async function fetchData() {
       await fetch('/api/samesite/default');
@@ -17,6 +13,11 @@ export default function Home() {
     }
     fetchData();
   }, []);
+
+  // Render loading state while fetching
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
